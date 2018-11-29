80c10b682e97642267e9caae9df02e79a2961cc8
Benjamin Le Rohellec <benjamin@lerohellec.nom.fr>
2018-11-29 14:10:12 +0100

    [Navigateur] Introduction du navigateur sans les fonctions de mouvements

---
+++ src/navigation/navigateur.hpp
@@ -0,0 +1,24 @@
+#include "../position.hpp"
+#include "../vecteur2D.hpp"
+
+#define NAV_PRECISION 0.01f // Précision de la position en mètres
+
+/**
+ * Classe pour le navigateur.
+ * Celui-ci est capable de piloter le robot par mouvement élémentaire pour le conduire jusqu'au point donné.
+ */
+class Navigateur
+{
+    public:
+        Navigateur(Position* position);
+        /**
+         * Définit "cible" comme nouveau point à atteindre par le navigateur.
+         */
+        void chg_destination(Vecteur2D* cible);
+        void update();
+
+    private:
+        Position* position;
+        Vecteur2D* cible;
+        float angle;
+};
