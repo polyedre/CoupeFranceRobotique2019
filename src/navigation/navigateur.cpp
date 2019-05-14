613dba99aa92d0a1e8625d1703dce6f9abc9fd8d
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-05-14 10:16:26 +0200

    [Modif] Odométrie améliorée + fix updatePos

--- src/navigation/navigateur.cpp
+++ src/navigation/navigateur.cpp
@@ -1,161 +1,158 @@
-#include "navigateur.hpp"
-#include <stdio.h>
-#include <math.h>
 #include "../base.hpp"
 #include "../config.hpp"
+#include "navigateur.hpp"
+#include <cmath.h>
+#include <cstdio.h>
 
 extern int debug_monitor;
 extern int move;
 extern int running;
 
-Navigateur::Navigateur(Position *_position, PwmOut *_m_l, PwmOut *_m_r, DigitalOut *_d_l, DigitalOut *_d_r, Encoder * encod_l, Encoder * encod_r)
-{
-    position = _position;
+Navigateur::Navigateur(Position *_position, PwmOut *_m_l, PwmOut *_m_r,
+                       DigitalOut *_d_l, DigitalOut *_d_r, Encoder *encod_l,
+                       Encoder *encod_r) {
+  position = _position;
 
-    float p_vitesse = 4.5;
-    float k = 0.023;
-    // FIXME : Trouver bonnes valeurs de pid.
-    PIDDistance _pid_d(0.08, 0.001, 0.01, 0.03, 1, position);
-    PIDAngle _pid_a(0.01, 0.001, 0.001, 0.02, 0, position);
-    // PIDAngle _pid_a(0.03, 0.001, 0.001, 0.02, 0, position);
-    PIDVitesse _pid_v_l(p_vitesse * (1 - k), 0.001, 0, 0.001, 0, encod_l, 0.007);
-    PIDVitesse _pid_v_r(p_vitesse * (1 + k), 0.001, 0, 0.001, 0, encod_r, 0.007);
+  float p_vitesse = 4.5;
+  float k = 0.023;
+  // FIXME : Trouver bonnes valeurs de pid.
+  PIDDistance _pid_d(0.08, 0.001, 0.01, 0.03, 1, position);
+  PIDAngle _pid_a(0.01, 0.001, 0.001, 0.02, 0, position);
+  // PIDAngle _pid_a(0.03, 0.001, 0.001, 0.02, 0, position);
+  PIDVitesse _pid_v_l(p_vitesse * (1 - k), 0.001, 0, 0.001, 0, encod_l, 0.007);
+  PIDVitesse _pid_v_r(p_vitesse * (1 + k), 0.001, 0, 0.001, 0, encod_r, 0.007);
 
-    pid_d = _pid_d;
-    pid_a = _pid_a;
-    pid_v_l = _pid_v_l;
-    pid_v_r = _pid_v_r;
+  pid_d = _pid_d;
+  pid_a = _pid_a;
+  pid_v_l = _pid_v_l;
+  pid_v_r = _pid_v_r;
 
-    m_l = _m_l;
-    m_r = _m_r;
-    m_l->period(PWM_PERIOD);
-    m_r->period(PWM_PERIOD);
-    d_l = _d_l;
-    d_r = _d_r;
+  m_l = _m_l;
+  m_r = _m_r;
+  m_l->period(PWM_PERIOD);
+  m_r->period(PWM_PERIOD);
+  d_l = _d_l;
+  d_r = _d_r;
 }
 
-void Navigateur::set_destination(float x, float y)
-{
-    cible_x = x;
-    cible_y = y;
-    // Cible sert de stokage de la consigne
-    pid_d.setCommande(cible_x, cible_y);
+void Navigateur::set_destination(float x, float y) {
+  cible_x = x;
+  cible_y = y;
+  // Cible sert de stokage de la consigne
+  pid_d.setCommande(cible_x, cible_y);
 }
 
 void Navigateur::reset() {
-    position->reset();
-    pid_a.reset();
-    pid_d.reset();
-    set_destination(0, 0);
+  position->reset();
+  pid_a.reset();
+  pid_d.reset();
+  set_destination(0, 0);
 }
 
-void Navigateur::update()
-{
+void Navigateur::update() {
 
-    float x = position->get_x();
-    float y = position->get_y();
-    float theta = position->get_theta();
-    
-    // Initialisation des consignes
-    float angle_cons = 0;
-    float dist_cons = 0;
-    float angle_relatif = 0;
-    float angle_absolu_destination = theta;
-    int i = 1;
-    int dir_l;
-    int dir_r;
-    float cmr;
-    float cml;
+  float x = position->get_x();
+  float y = position->get_y();
+  float theta = position->get_theta();
 
+  // Initialisation des consignes
+  float angle_cons = 0;
+  float dist_cons = 0;
+  float angle_relatif = 0;
+  float angle_absolu_destination = theta;
+  int i = 1;
+  int dir_l;
+  int dir_r;
+  float cmr;
+  float cml;
 
-    int triggered = 0; // False
+  int triggered = 0; // False
 
-    // printf("x = %f, y = %f, theta=%f\n", x, y, theta);
-    // printf("%f, %f, %f\r\n", x, y, theta);
-    // printf("Accumulateurs : (Dist : %f) (Angle : %f)\r\n", pid_d.accumulateur, pid_a.accumulateur);
+  // printf("x = %f, y = %f, theta=%f\n", x, y, theta);
+  // printf("%f, %f, %f\r\n", x, y, theta);
+  // printf("Accumulateurs : (Dist : %f) (Angle : %f)\r\n", pid_d.accumulateur,
+  // pid_a.accumulateur);
 
-    float distance_cible = sqrt(carre(x - cible_x)+ carre(y - cible_y));
+  float distance_cible = sqrt(carre(x - cible_x) + carre(y - cible_y));
 
-    if (distance_cible > 0.1) {
-        angle_absolu_destination = modulo_angle_absolu(calculerAngle(x, y, cible_x, cible_y));
-    } else {
-        angle_absolu_destination = theta;
-    }
+  if (distance_cible > 0.1) {
+    angle_absolu_destination =
+        modulo_angle_absolu(calculerAngle(x, y, cible_x, cible_y));
+  } else {
+    angle_absolu_destination = theta;
+  }
 
-    angle_relatif = modulo_angle_relatif(angle_absolu_destination - theta);
+  angle_relatif = modulo_angle_relatif(angle_absolu_destination - theta);
 
-    if ((abs(angle_relatif) < 0.3) || (abs(abs(angle_relatif) - PI) < 0.3)) {
-        triggered = 1;
-        dist_cons = pid_d.getConsigne();
-    }
+  if ((abs(angle_relatif) < 0.3) || (abs(abs(angle_relatif) - PI) < 0.3)) {
+    triggered = 1;
+    dist_cons = pid_d.getConsigne();
+  }
 
-    if ((angle_relatif < PI + 0.3) && (angle_relatif > PI - 0.3)) i = -1;
+  if ((angle_relatif < PI + 0.3) && (angle_relatif > PI - 0.3))
+    i = -1;
 
-    //  Calcul Consigne pour angle avec angle cible rafraichie
-    if (i > 0) {
-        pid_a.setCommande(angle_absolu_destination);
-    } else {
-        pid_a.setCommande(modulo_angle_absolu(angle_absolu_destination + PI));
-    }
-    angle_cons = pid_a.getConsigne();
+  //  Calcul Consigne pour angle avec angle cible rafraichie
+  if (i > 0) {
+    pid_a.setCommande(angle_absolu_destination);
+  } else {
+    pid_a.setCommande(modulo_angle_absolu(angle_absolu_destination + PI));
+  }
+  angle_cons = pid_a.getConsigne();
 
-    dist_cons = min(dist_cons, 0.4f);
-    angle_cons = min(angle_cons, 0.4f);
+  dist_cons = min(dist_cons, 0.4f);
+  angle_cons = min(angle_cons, 0.4f);
 
-    dist_cons = max(dist_cons, -0.4f);
-    angle_cons = max(angle_cons, -0.4f);
+  dist_cons = max(dist_cons, -0.4f);
+  angle_cons = max(angle_cons, -0.4f);
 
-    /*
-      Initialisation des directions pour que les moteurs tournent dans
-      le même sens. On inverse ensuite selon la consigne lors de l'appel
-      à `limiter_consigne`.
-      */
-    if (i > 0) {
-        dir_r = 1;
-        dir_l = 0;
+  /*
+    Initialisation des directions pour que les moteurs tournent dans
+    le même sens. On inverse ensuite selon la consigne lors de l'appel
+    à `limiter_consigne`.
+    */
+  if (i > 0) {
+    dir_r = 1;
+    dir_l = 0;
 
-        cmr = dist_cons + angle_cons; // Consigne moteur droit
-        cml = dist_cons - angle_cons; // Consigne moteur gauche
-    }  else {
-        dir_r = 0;
-        dir_l = 1;
+    cmr = dist_cons + angle_cons; // Consigne moteur droit
+    cml = dist_cons - angle_cons; // Consigne moteur gauche
+  } else {
+    dir_r = 0;
+    dir_l = 1;
 
-        cmr = dist_cons - angle_cons; // Consigne moteur droit
-        cml = dist_cons + angle_cons; // Consigne moteur gauche
-    }
+    cmr = dist_cons - angle_cons; // Consigne moteur droit
+    cml = dist_cons + angle_cons; // Consigne moteur gauche
+  }
 
+  pid_v_r.setCommande(cmr);
+  float cmr_v = pid_v_r.getConsigne();
 
-    pid_v_r.setCommande(cmr);
-    float cmr_v = pid_v_r.getConsigne();
+  pid_v_l.setCommande(cml);
+  float cml_v = pid_v_l.getConsigne();
 
-    pid_v_l.setCommande(cml);
-    float cml_v = pid_v_l.getConsigne();
+  if (debug_monitor) {
+    print_pos();
+    printf("cx:%.2f cy:%.2f t:%.2f r:%.2f l:%.2f A:%.2f D:%.2f R:%.2f F:%s "
+           "I:%s T:%s ",
+           cible_x, cible_y, convert_degree(angle_absolu_destination), cmr, cml,
+           angle_cons, dist_cons, convert_degree(angle_relatif),
+           pid_d.actionFinished ? "T" : "F", i < 0 ? "Y" : "N",
+           triggered == 1 ? "Y" : "N");
+  }
 
-    if (debug_monitor) {
-        print_pos();
-        printf("cx:%.2f cy:%.2f t:%.2f r:%.2f l:%.2f A:%.2f D:%.2f R:%.2f F:%s I:%s T:%s ",
-            cible_x, cible_y, convert_degree(angle_absolu_destination),
-            cmr, cml,
-            angle_cons, dist_cons,
-            convert_degree(angle_relatif),
-            pid_d.actionFinished ? "T" : "F",
-            i < 0 ? "Y" : "N",
-            triggered == 1 ? "Y" : "N");
-    }
+  limiter_consigne(&cmr_v, &dir_r);
+  limiter_consigne(&cml_v, &dir_l);
 
-    limiter_consigne(&cmr_v, &dir_r);
-    limiter_consigne(&cml_v, &dir_l);
+  // printf("Consignes : (l : %f) (r : %f)\r\n", cmr, cml);
 
-    // printf("Consignes : (l : %f) (r : %f)\r\n", cmr, cml);
+  if (move) {
+    m_l->write(cml_v);
+    m_r->write(cmr_v);
+  }
 
-    if (move) {
-        m_l->write(cml_v);
-        m_r->write(cmr_v);
-    }
-
-    *d_r = dir_r;
-    *d_l = dir_l;
-
+  *d_r = dir_r;
+  *d_l = dir_l;
 }
 
 /*
@@ -163,98 +160,89 @@ void Navigateur::update()
  * il peut être positif ou négatif.
  * TODO : Tester cette fonction
  */
-void Navigateur::rotate_by(float angle)
-{
-    float theta = position->get_theta();
-    float angle_dest = modulo_angle_absolu(theta + angle);
+void Navigateur::rotate_by(float angle) {
+  float theta = position->get_theta();
+  float angle_dest = modulo_angle_absolu(theta + angle);
 
-    pid_a.setCommande(angle_dest);
+  pid_a.setCommande(angle_dest);
 
-    while (!pid_a.actionFinished) {
+  while (!pid_a.actionFinished) {
 
-        if (running) {
-            float consigne = pid_a.getConsigne();
+    if (running) {
+      float consigne = pid_a.getConsigne();
 
-            consigne = min(consigne, 0.4f);
+      consigne = min(consigne, 0.4f);
 
-            float cmr = consigne;
-            float cml = -consigne;
+      float cmr = consigne;
+      float cml = -consigne;
 
-            int dir_l = 0;
-            int dir_r = 1;
+      int dir_l = 0;
+      int dir_r = 1;
 
-            pid_v_r.setCommande(cmr);
-            float cmr_v = pid_v_r.getConsigne();
+      pid_v_r.setCommande(cmr);
+      float cmr_v = pid_v_r.getConsigne();
 
-            pid_v_l.setCommande(cml);
-            float cml_v = pid_v_l.getConsigne();
+      pid_v_l.setCommande(cml);
+      float cml_v = pid_v_l.getConsigne();
 
-            if (debug_monitor) {
-                print_pos();
-                printf("cx:%.2f cy:%.2f t:%.2f r:%.2f l:%.2f CA:%.2f ",
-                    cible_x, cible_y, convert_degree(angle_dest),
-                    cmr, cml, consigne);
-            }
+      if (debug_monitor) {
+        print_pos();
+        printf("cx:%.2f cy:%.2f t:%.2f r:%.2f l:%.2f CA:%.2f ", cible_x,
+               cible_y, convert_degree(angle_dest), cmr, cml, consigne);
+      }
 
-            limiter_consigne(&cmr, &dir_r);
-            limiter_consigne(&cml, &dir_l);
+      limiter_consigne(&cmr, &dir_r);
+      limiter_consigne(&cml, &dir_l);
 
-            if (move) {
-                m_l->write(cml_v);
-                m_r->write(cmr_v);
-            }
+      if (move) {
+        m_l->write(cml_v);
+        m_r->write(cmr_v);
+      }
 
-            *d_r = dir_r;
-            *d_l = dir_l;
-
-        }
+      *d_r = dir_r;
+      *d_l = dir_l;
     }
-    pid_a.reset();
-    m_l->write(0);
-    m_r->write(0);
+  }
+  pid_a.reset();
+  m_l->write(0);
+  m_r->write(0);
 }
 
 /*
  * Avance de `distance` mètres.
  * TODO : Tester cette fonction
  */
-void Navigateur::avancer(float distance)
-{
-    float x = position->get_x();
-    float y = position->get_y();
-    float theta = position->get_theta();
+void Navigateur::avancer(float distance) {
+  float x = position->get_x();
+  float y = position->get_y();
+  float theta = position->get_theta();
 
-    float new_x = x + distance * cos(theta);
-    float new_y = y + distance * sin(theta);
+  float new_x = x + distance * cos(theta);
+  float new_y = y + distance * sin(theta);
 
-    set_destination(new_x, new_y);
+  set_destination(new_x, new_y);
 
-    while (!pid_d.actionFinished) {
-        if (running) {
-            update();
-        }
-        else {
-            wait(0.5);
-        }
+  while (!pid_d.actionFinished) {
+    if (running) {
+      update();
+    } else {
+      wait(0.5);
     }
+  }
 
-    pid_d.reset();
-    m_l->write(0);
-    m_r->write(0);
+  pid_d.reset();
+  m_l->write(0);
+  m_r->write(0);
 }
 
-
-
-
-void Navigateur::print_pos()
-{
-    printf("\n\rx:%.2f y:%.2f t:%.2f ", position->get_x(), position->get_y(), convert_degree(position->get_theta()));
+void Navigateur::print_pos() {
+  printf("\n\rx:%.2f y:%.2f t:%.2f ", position->get_x(), position->get_y(),
+         convert_degree(position->get_theta()));
 }
 
-void Navigateur::updatePos()
-{
-    short vitesses[2] = {10, 10};
-    position->update(vitesses);
-    pid_v_r.updateVitesse(vitesses[0]);
-    pid_v_l.updateVitesse(vitesses[1]);
+void Navigateur::updatePos() {
+  short vitesses[2] = {0, 0};
+  position->update(vitesses);
+  pid_v_r.updateVitesse(vitesses[0]);
+  pid_v_l.updateVitesse(vitesses[1]);
 }
--- src/position.cpp
+++ src/position.cpp
@@ -1,78 +1,61 @@
+#include "base.hpp"
+#include "config.hpp"
+#include "encoder.hpp"
 #include "mbed.h"
 #include "position.hpp"
-#include "encoder.hpp"
-#include "config.hpp"
-#include "base.hpp"
 #include <cmath>
 
-// const float D = 13900; // 0.2200684f * 367.0f; // mm.tick^-1 * distance entre roues
+// const float D = 13900; // 0.2200684f * 367.0f; // mm.tick^-1 * distance entre
+// roues
 
-float sg(float val)
-{
-    return (val < 0.0f) ? -1.0f : 1.0f;
+float sg(float val) { return (val < 0.0f) ? -1.0f : 1.0f; }
+
+Position::Position() {}
+
+Position::Position(Encoder *_encod_l, Encoder *_encod_r) {
+  x = 0;
+  y = 0;
+  theta = 0;
+  encod_l = _encod_l;
+  encod_r = _encod_r;
 }
 
-Position::Position(){}
+float Position::get_x() { return x; }
 
-Position::Position(Encoder* _encod_l, Encoder* _encod_r)
-{
-    x = 0;
-    y = 0;
-    theta = 0;
-    encod_l = _encod_l;
-    encod_r = _encod_r;
-}
+float Position::get_y() { return y; }
 
-float Position::get_x()
-{
-    return x;
-}
-
-float Position::get_y()
-{
-    return y;
-}
-
-float Position::get_theta()
-{
-    return theta;
-}
+float Position::get_theta() { return theta; }
 
 /**
- * Nouveau modèle de déplacement élémentaire centré autour du milieu des deux encodeurs.
+ * Nouveau modèle de déplacement élémentaire centré autour du milieu des deux
+ * encodeurs.
  * */
-void Position::update(short vitesses[])
-{
-    short dl = encod_l->diff();
-    short dr = encod_r->diff();
+void Position::update(short vitesses[]) {
+  short dl = encod_l->diff();
+  short dr = encod_r->diff();
 
-    vitesses[0] = dr;
-    vitesses[1] = dl;
+  vitesses[0] = dr;
+  vitesses[1] = dl;
 
-    float del = dl / ENCODEUR_ECHELLE;     // mouvement de l'encodeur gauche en mètres
-    float der = dr / ENCODEUR_ECHELLE;    // mouvement de l'encodeur droit en mètres
+  float del = dl / ENCODEUR_ECHELLE; // mouvement de l'encodeur gauche en mètres
+  float der = dr / ENCODEUR_ECHELLE; // mouvement de l'encodeur droit en mètres
 
-    float dx = (del + der) / 2 * cos(theta);
-    float dy = (del + der) / 2 * sin(theta);
-    float dTh = (der - del) / DIAMETER;
+  // On avance de la motié de la longueur
+  x += (del + der) / 4 * cos(theta);
+  y += (del + der) / 4 * sin(theta);
 
-    // printf("(dx, del, der) = (%f, %f, %f)\n", dx, del, der);
+  // On tourne
+  theta = modulo_angle_absolu((der - del) / DIAMETER + theta);
 
-    // TODO : check la position du calcul de theta
-
-    x += dx;
-    y += dy;
-    theta = theta + dTh;
-    theta = modulo_angle_absolu(theta);
-
+  // On réavance de la motié de la longueur
+  x += (del + der) / 4 * cos(theta);
+  y += (del + der) / 4 * sin(theta);
 }
 
-void Position::init(){
-    reset();
-}
+void Position::init() { reset(); }
 
-void Position::reset(){
-    x = 0;
-    y = 0;
-    theta = 0;
+void Position::reset() {
+  x = 0;
+  y = 0;
+  theta = 0;
 }
